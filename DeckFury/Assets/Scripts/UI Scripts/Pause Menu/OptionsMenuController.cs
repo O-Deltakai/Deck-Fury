a525a0915a0104449b9ad65545f8738612bb2d3a
Matthew Qiu <deltakai2010@gmail.com>
2024-01-11 01:14:31 +1100

    set up menu layouts and tabs

---
+++ DeckFury/Assets/Scripts/UI Scripts/Pause Menu/OptionsMenuController.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class OptionsMenuController : MonoBehaviour
+{
+
+    [SerializeField] List<GameObject> menuLayouts;
+    [SerializeField] List<Button> tabButtons;
+
+    [SerializeField] GameObject currentLayout;
+    [SerializeField] Button currentSelectedButton;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+
+    public void SwitchLayout(GameObject layout)
+    {
+        if(currentLayout == layout) { return; }
+
+        if(!menuLayouts.Contains(layout)) 
+        {
+            Debug.LogWarning("The given object is not part of the menu layouts, the options menu cannot switch to given object.", layout); 
+            return; 
+        }
+
+        currentLayout.SetActive(false);
+        currentLayout = layout;
+        currentLayout.SetActive(true);
+    }
+
+    public void ToggleTabButtonState(Button button)
+    {
+        if(currentSelectedButton != null)
+        {
+            if(currentSelectedButton == button){ return; }
+        }
+
+        if(!tabButtons.Contains(button))
+        {
+            Debug.LogWarning("The given button is not part of the menu tab buttons, the options menu cannot switch to given button.", button); 
+            return; 
+        }
+
+        if(currentSelectedButton == null)
+        {
+            currentSelectedButton = button;
+        }else
+        {
+            currentSelectedButton.interactable = true;
+            currentSelectedButton = button;
+        }
+
+        currentSelectedButton.interactable = false;
+
+    }
+
+
+}
