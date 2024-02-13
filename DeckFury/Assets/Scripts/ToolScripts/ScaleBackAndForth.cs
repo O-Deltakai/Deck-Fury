d931b7b29c7d02c22c367400a8a476c623fc79aa
Matthew Qiu <deltakai2010@gmail.com>
2024-02-13 18:21:18 +1100

    Refactor ScaleBackAndForth script to include tweening and scale multiplication

--- DeckFury/Assets/Scripts/ToolScripts/ScaleBackAndForth.cs
+++ DeckFury/Assets/Scripts/ToolScripts/ScaleBackAndForth.cs
@@ -4,12 +4,14 @@ using UnityEngine;
 using DG.Tweening;
 
 /// <summary>
-/// Simple component that scales an object back and forth.
+/// Simple component that scales an object back and forth with tweening.
 /// </summary>
 public class ScaleBackAndForth : MonoBehaviour
 {
     [SerializeField] float _scaleDuration = 0.5f;
     [SerializeField] float _scaleAmount = 0.1f;
+
+    [Tooltip("If true, the initial scale will be multiplied by the scale amount.")]
     [SerializeField] bool _multiplyScale = false;
 
     [SerializeField] bool _scaleX = true;
@@ -31,45 +33,35 @@ public class ScaleBackAndForth : MonoBehaviour
 
     void ScaleForth()
     {
-        if (scaleTween.IsActive())
-        {
-            scaleTween.Kill();
-        }
+        if (scaleTween.IsActive()){ scaleTween.Kill(); }
+
+        Vector3 scaleAmount = new(_scaleX ? _scaleAmount : 0, _scaleY ? _scaleAmount : 0, 0);
 
         if(_multiplyScale)
         {
-
+            scaleAmount = new Vector3(_initialScale.x * _scaleAmount, _initialScale.y * _scaleAmount, 0);
+            scaleTween = transform.DOScale(scaleAmount, _scaleDuration).SetEase(_easeType).OnComplete(ScaleBack);
+        }else
+        {
+            scaleTween = transform.DOScale(transform.localScale + scaleAmount, _scaleDuration).SetEase(_easeType).OnComplete(ScaleBack);
         }
-
-        Vector3 scaleAmount = new(_scaleX ? _scaleAmount : 0, _scaleY ? _scaleAmount : 0, 0);
-        scaleTween = transform.DOScale(transform.localScale + scaleAmount, _scaleDuration).SetEase(_easeType).OnComplete(ScaleBack);
     }
 
     void ScaleBack()
     {
-        if (scaleTween.IsActive())
-        {
-            scaleTween.Kill();
-        }
+        if (scaleTween.IsActive()){ scaleTween.Kill(); } 
 
-        Vector3 scaleAmount = new(_scaleX ? _scaleAmount : 0, _scaleY ? _scaleAmount : 0, 0);
         scaleTween = transform.DOScale(_initialScale, _scaleDuration).SetEase(_easeType).OnComplete(ScaleForth);
     }
 
     void OnDestroy()
     {
-        if (scaleTween.IsActive())
-        {
-            scaleTween.Kill();
-        }
+        if (scaleTween.IsActive()){ scaleTween.Kill();} 
     }
 
     void OnDisable()
     {
-        if (scaleTween.IsActive())
-        {
-            scaleTween.Kill();
-        }
+        if (scaleTween.IsActive()){ scaleTween.Kill(); }
     }
 
 }
