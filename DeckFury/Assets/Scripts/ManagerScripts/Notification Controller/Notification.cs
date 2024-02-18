24198b27d8e29a5314e12b77baa35b378e7036ae
Matthew Qiu <deltakai2010@gmail.com>
2024-02-19 05:09:20 +1100

    beging building new notification system for informing player when a meta progression item as been unlocked

---
+++ DeckFury/Assets/Scripts/ManagerScripts/Notification Controller/Notification.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public struct Notification
+{
+    public NotificationType notificationType;
+    public string notificationTitle;
+    public string notificationDescription;
+}
